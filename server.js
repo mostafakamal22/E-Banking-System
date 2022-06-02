794e0fcd1360886ba6519e4b7c74dbfddb0ac427
mostafakamal22 <36553185+mostafakamal22@users.noreply.github.com>
2022-06-02 22:55:17 +0200

    intial files

---
+++ server.js
@@ -0,0 +1,35 @@
+require("dotenv").config();
+const express = require("express");
+
+const expressLayouts = require("express-ejs-layouts");
+
+const app = express();
+
+//set view engine as ejs engine
+app.set("view engine", "ejs");
+//set view directory
+app.set("views", __dirname + "/views");
+//set layout folder
+app.set("layout", "layouts/layout");
+//use express layout middleware
+app.use(expressLayouts);
+//application main folder
+app.use(express.static("public"));
+
+//use mongodb
+const mongoose = require("mongoose");
+//connecting to Mongodb
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connected..."))
+  .catch((err) => console.log(err));
+
+///    index router  ///
+const indexRouter = require("./routes/index");
+
+app.use("/", indexRouter);
+
+app.listen(process.env.Port || 3000, () => console.log("server running..."));
